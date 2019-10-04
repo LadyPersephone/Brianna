bdc377421522b5d3d907461de16a3f9ddc3a9931
Brianna Woodhead <Brianna_W@mamsoft.local>
2019-10-04 15:15:50 +0100

    save changes

--- e2e/page_objects/vol_objects/vol-pe.page.ts
+++ e2e/page_objects/vol_objects/vol-pe.page.ts
@@ -12,4 +12,14 @@ export class VolPartsEntryPage{
 
     public searchResultsRowSelector = "div.ui-grid-cell-contents.ng-binding.ng-scope";
 
+    public searchResultsVehicleTable = $("div#gridvehicleSearchGrid");
+
+    public searchResultsVehicleRowSelector = $("div#gridvehicleSearchGrid");
+
+    public searchVehicleSelectFromGrid = $("p.form-control-static.ng-binding");
+
+    public selectVehicleButton = $("button#btnSelect.btn.btn-primary.ng-binding");
+
+    public customerContactContinueButton = $("button#btnContinue.btn.btn-primary.ng-binding");
+
 }
